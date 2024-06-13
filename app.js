2eb9c699a15a1924284a42e48d3e98d24b44dad8
Uryel Farias <uryel.farias@gmail.com>
2024-06-12 22:50:29 -0300

    Melhorando o código com funções

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = `Jogo do número secreto`;
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = `Jogo do número secreto`;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = `Escolha um número entre 1 e 10`;
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = `Escolha um número entre 1 e 10`;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
