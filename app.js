23e71fd15d374f84081739c419f84a045581cbad
Uryel Farias <uryel.farias@gmail.com>
2024-06-13 23:40:29 -0300

    alterando o DOM da pagina principal.

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = `Jogo do número secreto`;
+let numeroSecreto = gerarNumeroAleaatorio;
 
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = `Escolha um número entre 1 e 10`;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+
 }
+
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou')
+        exibirTextoNaTela('p', Você descobriu o número numeroSecreto, parabéns! )
+    } else {
+        if(chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número sevreto é maior')
+        } else if (chute < numeroSecreto)  {
+            exibirTextoNaTela('p', 'O número secreto é 10')
+        }
+    }
+}
+
+
