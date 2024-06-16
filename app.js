49c3ff869c140d1948306b8a90f6be9c0b9eeb62
Uryel Farias <uryel.farias@gmail.com>
2024-06-15 23:37:57 -0300

    limpando o campo de digitação

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1)
-}
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
@@ -23,14 +20,23 @@ function verificarChute() {
         let palavraTentativa = numeroDeTentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número Secreto com ${numeroDeTentativas} ${palavraTentativa}, parabéns!`
         exibirTextoNaTela('p', mensagemTentativas )
+        document.getElementById('reiniciar').removeAttribute('disabled')
     } else {
         if(chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número sevreto é menor')
+            exibirTextoNaTela('p', 'O número secreto é menor')
         } else if (chute < numeroSecreto)  {
             exibirTextoNaTela('p', 'O número secreto é maior')
         }
         tentativas++;
+        limparCampo();
     }
 }
 
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1)
+}
 
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
