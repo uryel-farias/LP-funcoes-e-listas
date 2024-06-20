952a370d80d26b604de9b3ee2b5389e2946e25a7
Uryel Farias <uryel.farias@gmail.com>
2024-06-19 23:30:58 -0300

    limitando o tamanho da lista e limpando ela quando houver um novo jogo.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let numeroDeTentativas = 1;
 
@@ -38,7 +39,13 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
     if (listaDeNumerosSorteados.includes(numeroEscolhido)){
         return gerarNumeroAleatorio();
     } else  {
